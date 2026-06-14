5c7c90bff7cc7343ff8017a9967e78b71c30ee6d
unknown <kvinay21505@gmail.com>
2026-06-14 16:18:04 +0530

    implemented signup api

--- app/main.py
+++ app/main.py
@@ -3,6 +3,13 @@ from fastapi.templating import Jinja2Templates
 from app.database import engine, Base
 from app.models.user import User
 
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
+from app.dependencies import get_db
+from app.schemas.user import UserCreate, UserResponse
+from app.crud.user import create_user
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -37,3 +44,10 @@ def dashboard_page(request: Request):
         request=request,
         name="dashboard.html"
     )
+
+@app.post("/signup", response_model=UserResponse)
+def signup(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    return create_user(db, user)
